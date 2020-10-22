7810f0d678a6182414ca4ce0c0a7ee63aa8fee85
dungtv <dungtv@hblab.vn>
2020-10-22 08:48:55 +0700

    'aa'

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from "./js/dashboard/Dashboard";
+import SignIn from "./js/auth/SignIn";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <SignIn/>
     </div>
   );
 }
